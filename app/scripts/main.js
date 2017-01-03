4499b229e14a2875988a98276270998879c21b96
Chinmay Desai <chinmay@chinmay.com.au>
2017-01-03 15:41:37 +1100

    Created Add Task action

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,67 +1,19 @@
 var app = new Vue({
-    el: '#app',
+    el: '#root',
     data: {
-        message: 'Hello Vue.js!'
-    }
-});
-
-var app2 = new Vue({
-    el: '#app2',
-    data: {
-        message: 'Dynamically bound title.'
-    }
-});
-
-var app3 = new Vue({
-    el: '#app3',
-    data: {
-        seen: false
-    }
-});
-
-var app4 = new Vue({
-    el: '#app4',
-    data: {
+        task: 'Add a task',
+        status: false,
         todos: [
-            {text: 'make todo no 2'},
-            {text: 'make todo no 3'},
-            {text: 'make todo no 4'},
-            {text: 'check all todos as "done".'}
         ]
-    }
-});
-
-var app5 = new Vue({
-    el: '#app5',
-    data: {
-        message: 'Thank God I am not a palindrome.'
     },
+
     methods: {
-        reverseMessage: function(){
-            this.message = this.message.split('').reverse().join('');
-        }
-    }
-});
+      addTask: function(){
+        return this.todos.push(`{description: ${app.task}, status: false}`);
+      }
+    },
 
-var app6 = new Vue({
-    el: '#app6',
-    data: {
-        message: 'Hello Vue.js'
-    }
-});
+    computed: {
 
-Vue.component('todo-item', {
-    props: ['todoText'],
-    template: '<li>{{ todoText.text }}</li>'
-});
-
-var app7 = new Vue({
-    el: '#app7',
-    data: {
-        groceryList: [
-            {text: 'vegetables'},
-            {text: 'fruits'},
-            {text: 'tomatoes'}
-        ]
     }
 });
